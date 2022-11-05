a7f8f249ca569741a067ac051dfa9c6b4fd5ec9e
Vũ <voquangvu09112006@gmail.com>
2022-11-05 17:26:11 +0700

    upload files

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log(process.env.TOKEN)
+        
+const { Client, GatewayIntentBits, Partials, messageLink , EmbedBuilder } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildBans,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildVoiceStates,
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.User,
+        Partials.Role,
+        Partials.Emoji,
+        Partials.Invite,
+        Partials.Message,
+        Partials.GuildMember,
+        Partials.ThreadMember,
+        Partials.StageInstance,
+        Partials.ThreadChannel,
+    ]
+})
+const { TOKEN } = process.env.TOKEN
+
+
+
+client.login(TOKEN)
